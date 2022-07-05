e6bfc1b45581881c5ce3318bf1bbf454a2f8c985
smnet97 <sardor.mahkamov0701@gmail.com>
2022-07-05 14:41:59 +0500

    created News model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class News(models.Model):
+    subject = models.CharField(max_length=100)
+    content = models.TextField()
+    photo = models.FileField()
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        verbose_name = 'new'
+        verbose_name_plural = 'news'
+
+    
